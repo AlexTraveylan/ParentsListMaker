6d2ee894e24953478a8d40caab4fd7bd936614ca
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-26 00:59:16 +0200

    list of waiting parents on list

--- app/api/links/api.py
+++ app/api/links/api.py
@@ -57,6 +57,44 @@ def get_confirmed_parents_in_list(
         return result
 
 
+@links_api.get("/waiting/{list_id}", status_code=status.HTTP_200_OK)
+def get_waiting_parents_in_list(
+    list_id: int = Annotated[int, Path(title="list_id")],
+) -> list[ParentInformation]:
+    with unit_api("Tentative de récupérer les membres confirmés") as session:
+        parent_list = PARENTS_LIST_SERVICE.get_or_none(session, id=list_id)
+        if parent_list is None:
+            raise RessourceNotFoundException("La liste n'existe pas")
+
+        list_links = LIST_LINK_SERVICE.get_all_list_links_by_list_id(
+            session, parent_list.id
+        )
+
+        result: list[ParentInformation] = []
+        for list_link in list_links:
+            if list_link.status == UserOnListStatus.WAITING:
+                user_information = USER_INFORMATION_SERVICE.get_or_none(
+                    session, user_id=list_link.user_id
+                )
+
+                if user_information is None:
+                    raise RessourceNotFoundException(
+                        f"L'utilisateur {list_link.user_id} n'a pas d'informations"
+                    )
+
+                result.append(
+                    ParentInformation(
+                        first_name=user_information.first_name,
+                        last_name=user_information.name,
+                        position_in_list=list_link.position_in_list,
+                        is_email=False if user_information.email is None else True,
+                        is_admin=list_link.is_admin,
+                        is_creator=parent_list.creator_id == list_link.user_id,
+                    )
+                )
+        return result
+
+
 @links_api.patch("/up/{list_id}/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 def up_parent_position(
     admin_user: Annotated[
